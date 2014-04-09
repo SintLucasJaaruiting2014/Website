b65defcc82f5cfde8cd22eb40b76ad40e7f3460e
Roj Vroemen <me@rojvroemen.com>
2014-04-09 21:54:29 +0200

    Adding core service provider.

---
+++ app/src/SintLucas/Provider/CoreServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace SintLucas\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use SintLucas\Core\ParserManager;
+
+class CoreServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if($this->app->runningInConsole() === true)
+		{
+			$this->commands(
+				'importer.command'
+			);
+		}
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
