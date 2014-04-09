4f81a1f84a7d847ddd2ced3f28f38301e2fb929a
Roj Vroemen <me@rojvroemen.com>
2014-04-09 17:25:52 +0200

    Adding docblocks

--- app/src/SintLucas/School/Models/Program.php
+++ app/src/SintLucas/School/Models/Program.php
@@ -11,16 +11,31 @@ class Program extends Model {
 	 */
 	protected $table = 'school_programs';
 
+	/**
+	 * Belongs to relation with the location model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function location()
 	{
 		return $this->belongsTo('SintLucas\School\Models\Location');
 	}
 
+	/**
+	 * Belongs to relation with the type model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function type()
 	{
 		return $this->belongsTo('SintLucas\School\Models\Type');
 	}
 
+	/**
+	 * Belongs to relation with the year model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
 	public function year()
 	{
 		return $this->belongsTo('SintLucas\School\Models\Year');
