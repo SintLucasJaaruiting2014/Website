39e1c8feffb0aa5fa79bfc8db1f85569bd2f266a
Roj Vroemen <me@rojvroemen.com>
2014-04-09 16:06:18 +0200

    Adding repos for the remaining models.

---
+++ app/src/SintLucas/Profile/Repos/FilterOptionRepo.php
@@ -0,0 +1,14 @@
+<?php namespace SintLucas\Profile\Repos;
+
+use SintLucas\Core\Repos\EloquentRepo;
+
+class FilterOptionRepo extends EloquentRepo {
+
+	/**
+	 * Validation rules.
+	 *
+	 * @var array
+	 */
+	protected $rules = array();
+
+}
---
+++ app/src/SintLucas/Profile/Repos/FilterRepo.php
@@ -0,0 +1,14 @@
+<?php namespace SintLucas\Profile\Repos;
+
+use SintLucas\Core\Repos\EloquentRepo;
+
+class FilterRepo extends EloquentRepo {
+
+	/**
+	 * Validation rules.
+	 *
+	 * @var array
+	 */
+	protected $rules = array();
+
+}
